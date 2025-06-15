820a31e9fd8e5d6930964824ff876479a92abb6c
milan-with-code <milan.code.github@gmail.com>
2025-06-15 15:21:28 +0530

    remove console.log

--- packages/container/App.js
+++ packages/container/App.js
@@ -2,9 +2,6 @@ import React from 'react'
 import {mount} from "marketing/MarketingApp";
 import MarketingApp from './src/components/MarketingApp';
 
-console.log('mount :>> ', mount);
-
-
 const App = () => {
   return (
     <div>
